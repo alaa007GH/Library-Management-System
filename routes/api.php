bb820ba7bd115335ddb64b78f25df6c85ddc877d
Ali Assad <ali007developer@gmail.com>
2025-06-04 00:43:54 +0300

    auth and clear

--- routes/api.php
+++ routes/api.php
@@ -2,28 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TestController;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\BookController;
-use App\Http\Controllers\PurchaseController;
+use App\Http\Controllers\UserController;
 
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
 
-Route::get('test/get',[TestController::class,'get']);
-Route::get('product/get',[ProductController::class,'get']);
-Route::post('product/create',[ProductController::class,'create']);
-Route::post('test/create',[TestController::class,'create']);
-Route::post('product/update/{id}',[ProductController::class,'update']);
-Route::post('product/delete/{id}',[ProductController::class,'delete']);
-Route::put('test/update/{id}',[TestController::class,'update']);
-Route::delete('test/delete/{id}',[TestController::class,'delete']);
-Route::get('book/get',[BookController::class,'get']);
-Route::post('book/create',[BookController::class,'create']);
-Route::post('book/update/{id}',[BookController::class,'update']);
-Route::post('book/delete/{id}',[BookController::class,'delete']);
-
-
-Route::post('purchase/create',[PurchaseController::class,'create']);
+Route::post('register',[UserController::class,'register']);
+Route::post('login',[UserController::class,'login']);
+Route::post('logout',[UserController::class,'logout'])
+  ->middleware('auth:sanctum');
