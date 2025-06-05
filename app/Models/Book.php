f338456d0321d502e6f9556500f9a518497e2ac1
alaa007GH <lgh235234@gmail.com>
2025-06-05 17:18:32 +0300

    commit

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Book extends Model
 {
     protected $fillable = ['book_title','author','description','price','review','category_id'];
-}
+
+public function borrowedBook(){
+return $this->hasMany(BorrowedBook::class,'category_id');}
+
+public function users(){
+
+    return $this->belongsToMany(User::class, 'borrowed_books', 'book_id', 'user_id');
+
+
+
+}}
--- app/Models/BorrowedBook.php
+++ app/Models/BorrowedBook.php
@@ -7,5 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class BorrowedBook extends Model
 {
     protected $fillable = ['borrow_date','due_date','borrower_name','book_status'];
+    public function book(){
 
+    return $this->belongsTo(Book::class,'category_id');
+    }
 }
